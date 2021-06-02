009d6d8de49072e0cac58afec6e0ff3dc867ed5f
italoviana33 <italo_1002@live.com>
2021-06-02 17:33:15 -0300

    Create users model tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
-
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid if username is not a email' do
+    user = build(:user)
+    expect(user).to be_valid
+  end
+  it 'username is uniq' do
+    first_user = create(:user)
+    expect do
+      create(:user, username: first_user.username)
+    end.to raise_error
+  end
+  it 'is invalid if username have email format' do
+    user = build(:user, username: FFaker::Internet.email)
+    expect(user).to_not be_valid
+  end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,6 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
+require 'ffaker'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
@@ -41,7 +42,7 @@ RSpec.configure do |config|
 
   # You can uncomment this line to turn off ActiveRecord support entirely.
   # config.use_active_record = false
-
+  config.include FactoryBot::Syntax::Methods
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
   # `post` in specs under `spec/controllers`.
