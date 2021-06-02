f1318e83a03d4be52b4e9770ae2ad55cbea9b5f4
italoviana33 <italo_1002@live.com>
2021-06-02 12:41:18 -0300

    configure user to login with username

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :username, presence: true, uniqueness: { case_sensitive: false }
+  validates_format_of :username, with: /^[a-zA-Z0-9_.]*$/, multiline: true
+  #where(conditions).where(['username = :value OR lower(email) = lower(:value)', { value: login }]).first
+
+  attr_writer :login
+
+  def login
+    @login || username || email
+  end
 end
