4b65b5add0f9359c6d27684c8dda8b3829c28f82
italoviana33 <italo_1002@live.com>
2021-06-03 09:04:00 -0300

    Create login username and email tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,17 +1,33 @@
 require 'rails_helper'
 RSpec.describe User, type: :model do
-  it 'is valid if username is not a email' do
-    user = build(:user)
-    expect(user).to be_valid
+  describe 'attributes' do
+    it 'is valid if username is not a email' do
+      user = build(:user)
+      expect(user).to be_valid
+    end
+    it 'username is uniq' do
+      first_user = create(:user)
+      expect do
+        create(:user, username: first_user.username)
+      end.to raise_error(ActiveRecord::RecordInvalid, 'Validation failed: Username has already been taken')
+    end
+    it 'is invalid if username have email format' do
+      user = build(:user, username: FFaker::Internet.email)
+      expect(user).to_not be_valid
+    end
   end
-  it 'username is uniq' do
-    first_user = create(:user)
-    expect do
-      create(:user, username: first_user.username)
-    end.to raise_error
-  end
-  it 'is invalid if username have email format' do
-    user = build(:user, username: FFaker::Internet.email)
-    expect(user).to_not be_valid
+
+  describe 'login' do
+    before(:each) do
+      @user = create(:user, password: '123456')
+    end
+    it 'with username' do
+      logged_user = User.find_for_database_authentication({ login: @user.username })
+      expect(logged_user).to eq(@user)
+    end
+    it 'with email' do
+      logged_user = User.find_for_database_authentication({ login: @user.email })
+      expect(logged_user).to eq(@user)
+    end
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,11 +1,14 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
+
 require 'ffaker'
+
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
-abort("The Rails environment is running in production mode!") if Rails.env.production?
+abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
+require 'devise'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -39,7 +42,8 @@ RSpec.configure do |config|
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
-
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Devise::Test::IntegrationHelpers, type: :controller
   # You can uncomment this line to turn off ActiveRecord support entirely.
   # config.use_active_record = false
   config.include FactoryBot::Syntax::Methods
